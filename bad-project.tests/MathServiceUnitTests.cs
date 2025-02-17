02868965ce771d23ace7d81d3328e135eede9f87
Lars Nysom <LANY@ucn.dk>
2025-02-17 12:28:31 +0100

    Added unit tests with moq

--- bad-project.tests/MathServiceUnitTests.cs
+++ bad-project.tests/MathServiceUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,18 @@ namespace bad_project.tests
     public class MathServiceUnitTests
     {
         [Fact]
-        public void CalculateSum_ReturnsCorrectResult()
+        public void CalculateSumWithMoq_ReturnsCorrectResult()
+        {
+            var mockCalculator = new Mock<ICalculator>();
+            mockCalculator.Setup(c => c.Add(It.IsAny<int>(), It.IsAny<int>())).Returns(16);
+
+            var mathService = new MathService(mockCalculator.Object);
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            Assert.Equal(15, mathService.CalculateSum(numbers)); // Fejlen her: 1+2+3+4+5=15, men er det det der returneres?
+        }
+
+        [Fact]
+        public void CalculateSumWithFake_ReturnsCorrectResult()
         {
             var calculator = new FakeCalculator();
             var mathService = new MathService(calculator);
